a9e36c07d1af239ae9edc13548760719845fc603
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 18:33:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.onload=()=>{
-  const randomNum = (()=>{
+window.onload= () => {
+  const randomNum = (()=> {
     const min=1;
     const max =100;
      return Math.floor(Math.random()*(max-min+1))+min;
   
-  });
+  })();
   document.getElementById("result").textContent=randomNum;
 };
